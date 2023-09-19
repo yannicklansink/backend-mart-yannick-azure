c18f51b3a0d2062e45185c40647421a1c4dca20f
Yannick Lansink <yannick.lansink@live.nl>
2023-09-19 15:33:18 +0200

    backend

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UserController : Controller
+    {
+        public UserController() { }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] UserFormModel userForm)
+        {
+            Console.WriteLine($"First Name: {userForm.FirstName}");
+            Console.WriteLine($"Last Name: {userForm.LastName}");
+            Console.WriteLine($"License Plate: {userForm.LicensePlate}");
+            //Console.WriteLine($"Picture Length: {userForm.Picture.Length}");
+
+            return Ok(new { message = "Data received" });
+        }
+
+        public class UserFormModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string LicensePlate { get; set; }
+            public string? Picture { get; set; } 
+        }
+
+
+    }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,50 @@
+
+namespace backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("https://agreeable-meadow-0c593c303.3.azurestaticapps.net/",
+                                                          "http://localhost:4200")
+                                                            .AllowAnyMethod()
+                                                            .AllowAnyHeader(); 
+                                  });
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
